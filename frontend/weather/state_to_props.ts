35b3c2c60dcc221afd48d933f118f20c5fb77606
Pablo C (Takkär) <pablocdu22@gmail.com>
2020-01-09 11:26:17 +0100

    Fix errors

--- frontend/weather/state_to_props.ts
+++ frontend/weather/state_to_props.ts
@@ -4,6 +4,13 @@ import {
     selectAllSensors,
     selectAllSensorReadings
 } from "../resources/selectors";
+import { Everything } from "../interfaces";
+import { TaggedSensor, TaggedSensorReading } from "farmbot";
+
+interface Props {
+    sensors: TaggedSensor[];
+    sensorReadings: TaggedSensorReading[];
+}
 
 export function mapStateToProps(props: Everything): Props {
     return {
